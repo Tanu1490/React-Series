fd09a9a1420d81852240a50348caec13b6b5ff8f
Tanu1490 <tanushree110803@gmail.com>
2026-03-17 19:07:34 +0530

    initial project setup

---
+++ React-Basics/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./Chai"
+
+function App() {
+  return(
+    <>
+     <h1>Learning how to render other jsx in here</h1>
+     <Chai/>
+     <h3>Weather is Beautiful today.</h3>
+    </>
+    
+    // we can only return one element at a time 
+    // for multiple elements to return we can wrap them in div and instead of writing div multiple time we can use <> </> - fragments
+  )
+}
+
+export default App
